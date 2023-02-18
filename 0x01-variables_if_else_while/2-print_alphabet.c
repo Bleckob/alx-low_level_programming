575d54283ae56550430555bfd8d0c4113d379ebb
scoobydoo <myemail123@gmail.com>
2023-02-17 17:26:35 -0800

    variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: a program that writes alphabets in lowercase
+*
+* Return: always return 0 (success)
+*/
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar (alp);
+	putchar ('\n');
+	return (0);
+}
