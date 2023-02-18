f114e10186fb81bdb253e4ebfd61fa1da1aa4f1f
scoobydoo <myemail123@gmail.com>
2023-02-17 19:28:26 -0800

    variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: a program that writes alphabets in lowercase
+*
+* Return: always return 0 (success)
+*/
+
+int main(void)
+{
+	int  alp;
+
+	for (alp = 'z'; alp <= 'a'; alp++)
+	{
+		putchar (alp + 'z');
+	}
+	putchar ('\n');
+	return (0);
+	getch()
+}
