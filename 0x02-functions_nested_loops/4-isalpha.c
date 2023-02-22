9832714e942e90580c60ca316048463567008a48
scoobydoo <myemail123@gmail.com>
2023-02-21 18:31:07 -0800

    nested

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  * Return: 1 if char is a letter either lowercase or uppercase, otherwise 0
  */
 
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
